49197d2b958654f2bf6246b88f0d21fe054a89a9
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-12-23 14:39:27 -0800

    todoService.js

--- frontend/src/features/todo/todoService.js
+++ frontend/src/features/todo/todoService.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
 export const fetchTodos = async() => {
-    const response = await axios.get("/api/todo/");
+    const response = await axios.get("https://to-do-7682.onrender.com/api/todo/");
     return response.data;
 };
 
 
 export const addTodo = async(FormData) => {
-    const response = await axios.post("/api/todo/",FormData)
+    const response = await axios.post("https://to-do-7682.onrender.com/api/todo/",FormData)
    return response.data;
 };
 
 export const deleteTodo = async(id) => {
-    const response = await axios.delete("/api/todo/" + id);
+    const response = await axios.delete("https://to-do-7682.onrender.com/api/todo/" + id);
     // console.log(response.data)
     return response.data;
 }
 
 export const update = async(updatedTodo) => {
-    const response = await axios.put("/api/todo/"+ updatedTodo._id , updatedTodo);
+    const response = await axios.put("https://to-do-7682.onrender.com/api/todo/"+ updatedTodo._id , updatedTodo);
     // console.log(response.data)
     return response.data
 }
